43d94d2caa49311971070508a959dac6da192e4a
Sohailaabdelaziz266 <sohailolaabdelaziz@gmail.com>
2026-08-05 00:40:22 +0300

    Fix cpplint style, indents and header guard format

--- SWB-04/sohila/inc/static_array.h
+++ SWB-04/sohila/inc/static_array.h
@@ -1,26 +1,27 @@
 // Copyright 2026 Sohaila Abdelaziz
-#ifndef STATIC_ARRAY_H
-#define STATIC_ARRAY_H
+
+#ifndef SWB_04_SOHILA_INC_STATIC_ARRAY_H_
+#define SWB_04_SOHILA_INC_STATIC_ARRAY_H_
 
 #include <iostream>
 
 class StaticArray {
-private:
-    int data[100]; 
-    int size;    
+ private:
+    int data[100];
+    int size;
 
-public:
+ public:
     StaticArray() {
         size = 0;
     }
 
-   
     void insert(int value) {
         if (size < 100) {
             data[size] = value;
             size++;
         }
     }
+
     void display() {
         for (int i = 0; i < size; i++) {
             std::cout << data[i] << " ";
@@ -29,4 +30,5 @@ public:
     }
 };
 
-#endif
+#endif  // SWB_04_SOHILA_INC_STATIC_ARRAY_H_
+
