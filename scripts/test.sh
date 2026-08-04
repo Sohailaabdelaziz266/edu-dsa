c79dfb0fd7402fe9262306f3135439c3b888f500
Sohailaabdelaziz266 <sohailolaabdelaziz@gmail.com>
2026-08-04 23:24:31 +0300

    Add my updates

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,16 +6,21 @@ cd "$ROOT_DIR"
 
 ASSIGNMENT="${1:-SWB-04}"
 STUDENT_FILTER="${2:-}"
-BUILD_DIR="${ROOT_DIR}/build/${ASSIGNMENT}"
 
-"${ROOT_DIR}/scripts/build.sh" "$ASSIGNMENT"
+
+if [[ -n "$STUDENT_FILTER" ]]; then
+    make test MAIN_DIR="${ASSIGNMENT}/${STUDENT_FILTER}"
+else
+    make test MAIN_DIR="${ASSIGNMENT}"
+fi
+
 
 if [[ -n "$STUDENT_FILTER" ]]; then
     echo "Running tests for submission id: ${STUDENT_FILTER}"
-    ctest --test-dir "$BUILD_DIR" -R "_${STUDENT_FILTER}$" --output-on-failure
+    make run_tests MAIN_DIR="${ASSIGNMENT}/${STUDENT_FILTER}"
 else
     echo "Running tests for all submissions in assignment=${ASSIGNMENT}"
-    ctest --test-dir "$BUILD_DIR" --output-on-failure
+    make run_tests MAIN_DIR="${ASSIGNMENT}"
 fi
 
 echo "All selected tests passed."
